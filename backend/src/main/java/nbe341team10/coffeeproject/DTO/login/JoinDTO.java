0109d7dfbb2a2cfdb051d100c637d0c15adfe4d7
wonseokyoon <01088115030a@gmail.com>
2025-02-20 17:01:56 +0900

    test: join

--- backend/src/main/java/nbe341team10/coffeeproject/DTO/login/JoinDTO.java
+++ backend/src/main/java/nbe341team10/coffeeproject/DTO/login/JoinDTO.java
@@ -2,9 +2,11 @@ package nbe341team10.coffeeproject.DTO.login;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class JoinDTO {
     private String username;
     private String email;
--- backend/src/test/java/nbe341team10/coffeeproject/CoffeeprojectApplicationTests.java
+++ backend/src/test/java/nbe341team10/coffeeproject/CoffeeprojectApplicationTests.java
@@ -1,13 +1,50 @@
 package nbe341team10.coffeeproject;
 
+import nbe341team10.coffeeproject.DTO.login.JoinDTO;
+import nbe341team10.coffeeproject.domain.user.Role;
+import nbe341team10.coffeeproject.domain.user.UserEntity;
+import nbe341team10.coffeeproject.repository.UserRepository;
+import nbe341team10.coffeeproject.service.login.LoginService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@Transactional
 @SpringBootTest
 class CoffeeprojectApplicationTests {
 
+	@Autowired
+	private MockMvc mvc;
+	@Autowired
+	private LoginService loginService;
+    @Autowired
+    private UserRepository userRepository;
+
 	@Test
-	void contextLoads() {
+	@DisplayName("가입 성공 확인")
+	void join() throws Exception {
+		JoinDTO joinDTO = new JoinDTO();
+		joinDTO.setUsername("testuser");
+		joinDTO.setEmail("testuser@example.com");
+		joinDTO.setPassword("password123");
+		joinDTO.setAddress("Test Address");
+
+		UserEntity user=loginService.join(joinDTO);
+
+		assertEquals("testuser", user.getUsername());
+		assertEquals("testuser@example.com", user.getEmail());
+		assertEquals("Test Address", user.getAddress());
 	}
 
 }
