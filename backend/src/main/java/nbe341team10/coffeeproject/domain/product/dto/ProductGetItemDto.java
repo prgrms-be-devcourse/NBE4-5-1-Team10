1d062634b3f3b97a26e216a0ee2ae0ca3c30a495
2hwayoung <69628269+2hwayoung@users.noreply.github.com>
2025-02-20 21:34:02 +0900

    Feat/product/#5 Product Detail API (#26)
    * feat: add get productDetail api
    
    * test: add getItem notFound test code
    
    * rename: rename product dto with convention

---
+++ backend/src/main/java/nbe341team10/coffeeproject/domain/product/dto/ProductGetItemDto.java
@@ -0,0 +1,26 @@
+package nbe341team10.coffeeproject.domain.product.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import nbe341team10.coffeeproject.domain.product.entity.Product;
+
+
+@Getter
+@AllArgsConstructor
+public class ProductGetItemDto {
+    private Long id;
+    private String name;
+    private String description;
+    private int price;
+    private String imageUrl;
+    private int stockQuantity;
+
+    public ProductGetItemDto(Product product) {
+        this.id = product.getId();
+        this.name = product.getName();
+        this.description = product.getDescription();
+        this.price = product.getPrice();
+        this.imageUrl = product.getImageUrl();
+        this.stockQuantity = product.getStockQuantity();
+    }
+}
--- backend/src/test/java/nbe341team10/coffeeproject/product/ApiV1ProductControllerTest.java
+++ backend/src/test/java/nbe341team10/coffeeproject/product/ApiV1ProductControllerTest.java
@@ -34,7 +34,6 @@ public class ApiV1ProductControllerTest {
     @Autowired
     private ProductService productService;
 
-
     private void checkProducts(ResultActions resultActions, List<Product> products) throws Exception {
         for(int i = 0; i < products.size(); i++) {
             Product product = products.get(i);
@@ -42,7 +41,6 @@ public class ApiV1ProductControllerTest {
                 .andExpect(jsonPath("$.data.items[%d]".formatted(i)).exists())
                 .andExpect(jsonPath("$.data.items[%d].id".formatted(i)).value(product.getId()))
                 .andExpect(jsonPath("$.data.items[%d].name".formatted(i)).value(product.getName()))
-                .andExpect(jsonPath("$.data.items[%d].description".formatted(i)).value(product.getDescription()))
                 .andExpect(jsonPath("$.data.items[%d].imageUrl".formatted(i)).value(product.getImageUrl()))
                 .andExpect(jsonPath("$.data.items[%d].price".formatted(i)).value(product.getPrice()))
                 .andExpect(jsonPath("$.data.items[%d].stockQuantity".formatted(i)).value(product.getStockQuantity()));
@@ -70,6 +68,58 @@ public class ApiV1ProductControllerTest {
 
         List<Product> products = productService.getItems();
         checkProducts(resultActions, products);
+    }
+
+    private void checkProduct(ResultActions resultActions, Product product) throws Exception {
+        resultActions
+            .andExpect(jsonPath("$.data").exists())
+            .andExpect(jsonPath("$.data.id").value(product.getId()))
+            .andExpect(jsonPath("$.data.name").value(product.getName()))
+            .andExpect(jsonPath("$.data.description").value(product.getDescription()))
+            .andExpect(jsonPath("$.data.imageUrl").value(product.getImageUrl()))
+            .andExpect(jsonPath("$.data.price").value(product.getPrice()))
+            .andExpect(jsonPath("$.data.stockQuantity").value(product.getStockQuantity()));
+    }
+
+    ResultActions itemRequest(long id) throws Exception {
+        return mvc
+                .perform(get("/api/v1/products/%d".formatted(id)))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("상품 상세정보 조회")
+    void item() throws Exception {
+//        Given 사용자가 특정 상품을 선택하면
+//        When 시스템이 해당 상품의 상세 정보를 조회하여 제공하면
+//        Then 사용자는 상품명, 설명, 가격, 리뷰 등을 확인할 수 있다.
+        long productId = 1;
+
+        ResultActions resultActions = itemRequest(productId);
+
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(handler().handlerType(ApiV1ProductController.class))
+                .andExpect(handler().methodName("getItem"))
+                .andExpect(jsonPath("$.code").value("200-1"))
+                .andExpect(jsonPath("$.msg").value("Product retrieved successfully."));
+
+        Product product = productService.getItem(productId).get();
+        checkProduct(resultActions, product);
+    }
+
+    @Test
+    @DisplayName("상품 상세정보 조회 - 존재하지 않는 상품 조회")
+    void item2() throws Exception {
+       long productId = -1;
+        ResultActions resultActions = itemRequest(productId);
+
+        resultActions
+                .andExpect(status().isNotFound())
+                .andExpect(handler().handlerType(ApiV1ProductController.class))
+                .andExpect(handler().methodName("getItem"))
+                .andExpect(jsonPath("$.code").value("404-1"))
+                .andExpect(jsonPath("$.msg").value("Product not found."));
     }
 
 }
