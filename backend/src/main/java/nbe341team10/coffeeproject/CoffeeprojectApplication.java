0d5d3438fa8cfc03f3c6f86449dda912d58463a9
2hwayoung <hwa3270@gmail.com>
2025-02-20 12:25:00 +0900

    build: fix java springboot project initialize

---
+++ backend/src/main/java/nbe341team10/coffeeproject/CoffeeprojectApplication.java
@@ -0,0 +1,13 @@
+package nbe341team10.coffeeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeprojectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/nbe341team10/coffeeproject/CoffeeprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package nbe341team10.coffeeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
