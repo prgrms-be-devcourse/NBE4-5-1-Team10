68862440e1cb3297cc2fcd616b0c15873fdad2c2
2hwayoung <hwa3270@gmail.com>
2025-02-20 19:15:29 +0900

    build: add PR Test ci workflow for springboot test pass

--- backend/src/test/java/nbe341team10/coffeeproject/CoffeeprojectApplicationTests.java
+++ backend/src/test/java/nbe341team10/coffeeproject/CoffeeprojectApplicationTests.java
@@ -2,8 +2,10 @@ package nbe341team10.coffeeproject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CoffeeprojectApplicationTests {
 
 	@Test
