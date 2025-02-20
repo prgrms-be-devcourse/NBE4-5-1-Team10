bdebab5681df5e8e9eebfec473345104bf6a3a69
junho1131 <45115780+junho1131@users.noreply.github.com>
2025-02-20 10:41:26 +0900

    ㅡfeat/초기세팅

---
+++ backend/src/main/java/org/example/nbe451team10coffeproject/Nbe451Team10CoffeprojectApplication.java
@@ -0,0 +1,13 @@
+package org.example.nbe451team10coffeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nbe451Team10CoffeprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nbe451Team10CoffeprojectApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/org/example/nbe451team10coffeproject/Nbe451Team10CoffeprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.nbe451team10coffeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nbe451Team10CoffeprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
