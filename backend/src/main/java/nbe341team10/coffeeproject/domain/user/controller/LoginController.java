6bf3ab5ff51bfe758cc724c9833eda170957e8ed
wonseokyoon <01088115030a@gmail.com>
2025-02-21 11:49:17 +0900

    fix:conflict resolve

---
+++ backend/src/main/java/nbe341team10/coffeeproject/domain/user/controller/LoginController.java
@@ -0,0 +1,52 @@
+package nbe341team10.coffeeproject.domain.user.controller;
+
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import nbe341team10.coffeeproject.domain.user.dto.UserJoinRequest;
+import nbe341team10.coffeeproject.domain.user.dto.UserResponse;
+import nbe341team10.coffeeproject.domain.user.entity.Users;
+import nbe341team10.coffeeproject.domain.user.repository.UserRepository;
+import nbe341team10.coffeeproject.domain.user.service.LoginService;
+import nbe341team10.coffeeproject.global.dto.RsData;
+import nbe341team10.coffeeproject.global.exception.ServiceException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/user")
+public class LoginController {
+
+    private final UserRepository userRepository;
+    private final LoginService loginService;
+
+
+    // 회원가입
+    @PostMapping("/join")
+    public ResponseEntity<RsData<UserResponse>> Join(@Valid @RequestBody UserJoinRequest dto) {
+        // 중복 검사
+        if(loginService.checkName(dto.username())){
+            throw new ServiceException("400-1","Username already exists");
+        } else if (loginService.checkEmail(dto.email())) {
+            throw new ServiceException("400-1","Email is already in use");
+        }
+
+        Users user = loginService.join(dto);
+        UserResponse response=new UserResponse(user);
+
+        RsData<UserResponse> rsData=new RsData<>(
+                "200",
+                "success",
+                response
+        );
+
+        return ResponseEntity.ok(rsData);
+    }
+
+}
+
+
---
+++ backend/src/main/java/nbe341team10/coffeeproject/domain/user/service/LoginService.java
@@ -0,0 +1,45 @@
+package nbe341team10.coffeeproject.domain.user.service;
+
+import lombok.RequiredArgsConstructor;
+import nbe341team10.coffeeproject.domain.user.dto.UserJoinRequest;
+import nbe341team10.coffeeproject.domain.user.entity.Role;
+import nbe341team10.coffeeproject.domain.user.entity.Users;
+import nbe341team10.coffeeproject.domain.user.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    // 일반 회원가입
+    public Users join(UserJoinRequest userJoinRequest) {
+        String username = userJoinRequest.username();
+        String email = userJoinRequest.email();
+        String password = userJoinRequest.password();
+        String address = userJoinRequest.address();
+
+        Users user = Users.builder()
+                .username(username)
+                .email(email)
+                .password(bCryptPasswordEncoder.encode(password))
+                .address(address)
+                .role(Role.ROLE_USER)
+                .build();
+
+        userRepository.save(user);
+        return user;
+    }
+
+    public boolean checkName(String username) {
+        return userRepository.existsByUsername(username);
+    }
+    public boolean checkEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
+}
