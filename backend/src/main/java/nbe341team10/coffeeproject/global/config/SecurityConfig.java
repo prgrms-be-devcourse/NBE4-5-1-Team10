0f752dcf295b32da92dc50f9f8f67091beb51c2a
wonseokyoon <01088115030a@gmail.com>
2025-02-21 12:07:24 +0900

    fix:conflict resolve
    # Conflicts:
    #	.gitignore
    #	backend/src/main/java/nbe341team10/coffeeproject/global/config/SecurityConfig.java
    #	backend/src/main/resources/application.yml

--- backend/src/main/java/nbe341team10/coffeeproject/global/config/SecurityConfig.java
+++ backend/src/main/java/nbe341team10/coffeeproject/global/config/SecurityConfig.java
@@ -2,8 +2,6 @@ package nbe341team10.coffeeproject.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,7 +10,6 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
@@ -36,10 +33,6 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("api/**").permitAll()    // 접근 허용
-                        .requestMatchers("api/v1/admin/**").hasRole("ADMIN") // 관리자만
-                        .requestMatchers(HttpMethod.GET, "/api/*/products/**").permitAll()
-                        .requestMatchers("/api/*/cart/**").permitAll()
-                        .requestMatchers("api/*/user/**").permitAll()    // 접근 허용
                         .requestMatchers("/admin").hasRole("ADMIN") // 관리자만
                         .anyRequest().authenticated());
 
