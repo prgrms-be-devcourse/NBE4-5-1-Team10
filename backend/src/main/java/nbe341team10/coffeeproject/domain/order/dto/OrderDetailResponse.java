de1829b3c128168cbae445c1ccd0bf1a0ba589a8
HyungJunn <gudwns051613@gmail.com>
2025-02-23 14:33:50 +0900

    feat: get Order's Detail done

---
+++ backend/src/main/java/nbe341team10/coffeeproject/domain/order/dto/OrderDetailResponse.java
@@ -0,0 +1,23 @@
+package nbe341team10.coffeeproject.domain.order.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import nbe341team10.coffeeproject.domain.order.entity.OrderStatus;
+import nbe341team10.coffeeproject.domain.orderitem.dto.OrderItemDetailResponse;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class OrderDetailResponse {
+
+    private List<OrderItemDetailResponse> orderItems;
+
+    private OrderStatus orderStatus;
+
+    private String email;
+
+    private String address;
+}
