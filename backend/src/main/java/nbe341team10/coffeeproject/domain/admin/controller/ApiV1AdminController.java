ccef0ce4f6ff4208fe70d681d0292e35d9fa34d4
김형준 <gudwns051613@gmail.com>
2025-02-23 10:55:20 +0900

    fix: update import ApiV1AdminController.java

--- backend/src/main/java/nbe341team10/coffeeproject/domain/admin/controller/ApiV1AdminController.java
+++ backend/src/main/java/nbe341team10/coffeeproject/domain/admin/controller/ApiV1AdminController.java
@@ -8,11 +8,7 @@ import nbe341team10.coffeeproject.domain.product.service.ProductService;
 import nbe341team10.coffeeproject.global.dto.RsData;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
