cd6d67e813be69542252c9fdf60275cef851a12b
2hwayoung <hwa3270@gmail.com>
2025-02-24 02:11:32 +0900

    chore: rename CartDto and CartItemDto

---
+++ backend/src/main/java/nbe341team10/coffeeproject/domain/cart/dto/CartItemDto.java
@@ -0,0 +1,34 @@
+package nbe341team10.coffeeproject.domain.cart.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import nbe341team10.coffeeproject.domain.cart.entity.CartItem;
+import org.springframework.lang.NonNull;
+
+@Getter
+@NoArgsConstructor
+public class CartItemDto {
+    @NonNull
+    private long id;
+    @NonNull
+    private long productId;
+    @NonNull
+    private String productName;
+    private String productImageUrl;
+    @NonNull
+    private int quantity;
+    @NonNull
+    private int price;
+    @NonNull
+    private int totalPrice;
+
+    CartItemDto(CartItem cartItem) {
+        this.id = cartItem.getId();
+        this.productId = cartItem.getProduct().getId();
+        this.productName = cartItem.getProduct().getName();
+        this.productImageUrl = cartItem.getProduct().getImageUrl();
+        this.quantity = cartItem.getQuantity();
+        this.price = cartItem.getProduct().getPrice();
+        this.totalPrice = cartItem.getProduct().getPrice() * this.quantity;
+    }
+}
