096c45d1836b3ae0754a5f1efda46051dc20ff9d
wonseokyoon <134195472+wonseokyoon@users.noreply.github.com>
2025-02-24 13:00:24 +0900

    refactor:Update Refresh.java

--- backend/src/main/java/nbe341team10/coffeeproject/domain/user/entity/Refresh.java
+++ backend/src/main/java/nbe341team10/coffeeproject/domain/user/entity/Refresh.java
@@ -1,5 +1,6 @@
 package nbe341team10.coffeeproject.domain.user.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
