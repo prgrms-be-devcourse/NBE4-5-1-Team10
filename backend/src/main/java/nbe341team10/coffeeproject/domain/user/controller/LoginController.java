e0cbf174916ac91c458880d699129a6d6e1893fc
HyungJunn <gudwns051613@gmail.com>
2025-02-25 09:39:28 +0900

    docs: swagger add summary

--- backend/src/main/java/nbe341team10/coffeeproject/domain/user/controller/LoginController.java
+++ backend/src/main/java/nbe341team10/coffeeproject/domain/user/controller/LoginController.java
@@ -1,9 +1,8 @@
 package nbe341team10.coffeeproject.domain.user.controller;
 
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.ExpiredJwtException;
-import jakarta.servlet.http.Cookie;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
@@ -11,20 +10,17 @@ import lombok.RequiredArgsConstructor;
 import nbe341team10.coffeeproject.domain.jwt.JWTUtil;
 import nbe341team10.coffeeproject.domain.user.dto.UserJoinRequest;
 import nbe341team10.coffeeproject.domain.user.dto.UserResponse;
-import nbe341team10.coffeeproject.domain.user.entity.Refresh;
 import nbe341team10.coffeeproject.domain.user.entity.Users;
-import nbe341team10.coffeeproject.domain.user.repository.RefreshRepository;
-import nbe341team10.coffeeproject.domain.user.repository.UserRepository;
 import nbe341team10.coffeeproject.domain.user.service.LoginService;
 import nbe341team10.coffeeproject.global.dto.RsData;
 import nbe341team10.coffeeproject.global.exception.ServiceException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.util.Date;
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 @RestController
@@ -36,6 +32,7 @@ public class LoginController {
     private final JWTUtil jwtUtil;
 
     // 회원가입
+    @Operation(summary = "회원가입 하기")
     @PostMapping("/join")
     public ResponseEntity<RsData<UserResponse>> Join(@Valid @RequestBody UserJoinRequest dto) {
         // 중복 검사
@@ -59,6 +56,7 @@ public class LoginController {
 
     // 토큰 재발급
     // refresh 토큰으로 access 토큰 재발급
+    @Operation(summary = "refresh 토큰으로 access 토큰 재발급 하기")
     @PostMapping("/reissue")
     public RsData<?> reissue(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
